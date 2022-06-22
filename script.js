f792c351fa6b9f8efa1eb73c075711cc56c76ec4
Bxclusive <105201758+Bxclusive@users.noreply.github.com>
2022-06-22 07:57:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = "Usman Blessing Joy";
+let height = "1.90m";
+let country = "Nigeria";
+
+document.write("My name is " + name);
+document.write("<br> My Height is " + height);
+document.write("<br> My Country is " + country);
